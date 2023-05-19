40dd229153b1a2812c6166d22acd9925adc26a8c
MiniGan067 <MiniGan067@yandex.ru>
2023-05-19 17:53:32 +0300

    project done!

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from catalog.views import index
+from catalog.views import homepage, contacts
 
 urlpatterns = [
-    path('', index)
+    path('', homepage),
+    path('contacts/', contacts),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'catalog/index.html')
+def homepage(request):
+    return render(request, 'catalog/homepage.html')
+
+
+def contacts(request):
+    return render(request, 'catalog/contacts.html')
